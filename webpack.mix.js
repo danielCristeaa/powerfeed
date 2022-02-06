d554158c49b978b71dcee876acad45698b2615dc
Daniel Cristea <dani.cristea09@gmail.com>
2022-02-06 19:15:31 +0200

    implemented browsersync with autoreload; also updated laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,4 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js');
+mix.js('resources/js/app.js', 'public/js').vue();
+mix.browserSync({
+    host: "localhost",
+    proxy: 'webserver',
+    open: false,
+});
